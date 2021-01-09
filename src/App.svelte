216246a9e9303ea2a3ed71bf0fca0957391dbd5e
Matheus Alves <theuves@gmail.com>
2021-01-09 14:24:16 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Header from './components/Header.svelte';
+</script>
+
+<style>
+	main {
+		width: 900px;
+		margin: 0 auto;
+	}
+</style>
+
+<main>
+	<Header />
+</main>
---
+++ src/components/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let name = 'FGCalc';
+</script>
+
+<style>
+    .header {
+        display: flex;
+    }
+</style>
+
+<header class="header">
+    <div class="header-title-container">
+        <h1 class="header-title">
+            {name}
+        </h1>
+    </div>
+    <div class="header-actions-container">
+
+    </div>
+</header>
