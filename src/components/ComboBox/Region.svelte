6ff9a931b9af7af1185531c14fc64959bad27bde
Matheus Alves <theuves@gmail.com>
2021-01-09 20:53:17 -0400

    create Region.svelte

---
+++ src/components/ComboBox/Region.svelte
@@ -0,0 +1,16 @@
+<script>
+    import data from '../../utils/data.js'
+
+    const regions = data.map(value => ({
+        name: value.region,
+        description: value.description
+    }))
+</script>
+
+<select>
+    {#each regions as region}
+        <option value={region.name}>
+            {region.description}
+        </option>
+    {/each}
+</select>
