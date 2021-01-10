74ff9048b59a40e5b994d3645a58b773d3868f1a
Matheus Alves <theuves@gmail.com>
2021-01-09 20:52:50 -0400

    crate data.js with AWS informations

---
+++ src/utils/data.js
@@ -0,0 +1,178 @@
+export default [
+  {
+      'region': 'ca-central-1',
+      'description': 'Canada (Central)',
+      'prices': {
+          'cpu': 0.013368,
+          'ram': 0.0014595
+      }
+  },
+  {
+      'region': 'eu-west-1',
+      'description': 'Europe (Ireland)',
+      'prices': {
+          'cpu': 0.01257825,
+          'ram': 0.00138118
+      }
+  },
+  {
+      'region': 'eu-west-2',
+      'description': 'Europe (London)',
+      'prices': {
+          'cpu': 0.013968,
+          'ram': 0.001533
+      }
+  },
+  {
+      'region': 'sa-east-1',
+      'description': 'South America (Sao Paulo)',
+      'prices': {
+          'cpu': 0.0212609,
+          'ram': 0.00232159
+      }
+  },
+  {
+      'region': 'ap-northeast-2',
+      'description': 'Asia Pacific (Seoul)',
+      'prices': {
+          'cpu': 0.01422902,
+          'ram': 0.00156165
+      }
+  },
+  {
+      'region': 'af-south-1',
+      'description': 'Africa (Cape Town)',
+      'prices': {
+          'cpu': 0.01638,
+          'ram': 0.0018
+      }
+  },
+  {
+      'region': 'eu-west-3',
+      'description': 'Europe (Paris)',
+      'prices': {
+          'cpu': 0.01458,
+          'ram': 0.00159
+      }
+  },
+  {
+      'region': 'us-west-2',
+      'description': 'US West (Oregon)',
+      'prices': {
+          'cpu': 0.01269464,
+          'ram': 0.00139396
+      }
+  },
+  {
+      'region': 'ap-southeast-2',
+      'description': 'Asia Pacific (Sydney)',
+      'prices': {
+          'cpu': 0.014568,
+          'ram': 0.001596
+      }
+  },
+  {
+      'region': 'ap-northeast-1',
+      'description': 'Asia Pacific (Tokyo)',
+      'prices': {
+          'cpu': 0.01981009,
+          'ram': 0.00216673
+      }
+  },
+  {
+      'region': 'us-gov-east-1',
+      'description': 'AWS GovCloud (US-East)',
+      'prices': {
+          'cpu': 0.01458,
+          'ram': 0.00159
+      }
+  },
+  {
+      'region': 'us-gov-west-1',
+      'description': 'AWS GovCloud (US-West)',
+      'prices': {
+          'cpu': 0.01458,
+          'ram': 0.00159
+      }
+  },
+  {
+      'region': 'eu-north-1',
+      'description': 'Europe (Stockholm)',
+      'prices': {
+          'cpu': 0.01335,
+          'ram': 0.00147
+      }
+  },
+  {
+      'region': 'us-west-1',
+      'description': 'US West (Northern California)',
+      'prices': {
+          'cpu': 0.013968,
+          'ram': 0.001533
+      }
+  },
+  {
+      'region': 'ap-south-1',
+      'description': 'Asia Pacific (Mumbai)',
+      'prices': {
+          'cpu': 0.0133282,
+          'ram': 0.00145777
+      }
+  },
+  {
+      'region': 'us-east-2',
+      'description': 'US East (Ohio)',
+      'prices': {
+          'cpu': 0.01276353,
+          'ram': 0.00140153
+      }
+  },
+  {
+      'region': 'ap-east-1',
+      'description': 'Asia Pacific (Hong Kong)',
+      'prices': {
+          'cpu': 0.01668,
+          'ram': 0.00183
+      }
+  },
+  {
+      'region': 'eu-south-1',
+      'description': 'Europe (Milan)',
+      'prices': {
+          'cpu': 0.01458,
+          'ram': 0.00159
+      }
+  },
+  {
+      'region': 'ap-southeast-1',
+      'description': 'Asia Pacific (Singapore)',
+      'prices': {
+          'cpu': 0.015168,
+          'ram': 0.001659
+      }
+  },
+  {
+      'region': 'eu-central-1',
+      'description': 'Europe (Frankfurt)',
+      'prices': {
+          'cpu': 0.0140458,
+          'ram': 0.00154154
+      }
+  },
+  {
+      'region': 'us-east-1',
+      'description': 'US East (N. Virginia)',
+      'prices': {
+          'cpu': 0.01264355,
+          'ram': 0.00138835
+      }
+  },
+  {
+      'region': 'me-south-1',
+      'description': 'Middle East (Bahrain)',
+      'prices': {
+          'cpu': 0.01578,
+          'ram': 0.00174
+      }
+  }
+]
