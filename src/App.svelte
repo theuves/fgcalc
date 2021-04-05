2c672a081d8acba56097b4fd888b0ceff5b3d980
Matheus Alves <theuves@gmail.com>
2021-04-05 09:46:49 -0400

    disable exchange API

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,33 @@
 <script>
 	// Svelte
-	import {afterUpdate, onMount} from 'svelte'
+	import { afterUpdate, onMount } from "svelte";
 
 	// Utils
-	import createPriceGetter from './utils/createPriceGetter'
-	import getExchangeRates from './utils/getExchangeRates'
-	import formatPrice from './utils/formatPrice'
+	import createPriceGetter from "./utils/createPriceGetter";
+	// import getExchangeRates from "./utils/getExchangeRates";
+	import formatPrice from "./utils/formatPrice";
 
 	// Components
-	import Header from './components/Header/Header.svelte';
-	import TimeInput from './components/TimeInput.svelte'
-	import CPUInput from './components/CPUInput.svelte'
-	import RAMInput from './components/RAMInput.svelte'
-	import CapacityInput from './components/CapacityInput.svelte'
-	import Table from './components/Table.svelte'
+	import Header from "./components/Header/Header.svelte";
+	import TimeInput from "./components/TimeInput.svelte";
+	import CPUInput from "./components/CPUInput.svelte";
+	import RAMInput from "./components/RAMInput.svelte";
+	import CapacityInput from "./components/CapacityInput.svelte";
+	import Table from "./components/Table.svelte";
 
 	// Header inputs
-	let region = 'us-east-1'
-	let currency = 'USD'
+	let region = "us-east-1";
+	let currency = "USD";
 
 	// Form inputs
-	let cpu = 1
-    let ram = 2
-    let timeValue = 1
-	let timeType = 'month'
-	let capacityFargate = 1
-	let capacityFargateSpot = 0
+	let cpu = 1;
+	let ram = 2;
+	let timeValue = 1;
+	let timeType = "month";
+	let capacityFargate = 1;
+	let capacityFargateSpot = 0;
 
-	let currencyRates = {USD: '1.0'}
+	let currencyRates = { USD: "1.0" };
 
 	let prices = {
 		total: 0,
@@ -41,90 +41,55 @@
 			cpu: 0,
 			ram: 0,
 		},
-	}
+	};
 
-	function updateCurrencyRates() {
-		getExchangeRates().then(rates => {
-			console.info('[INFO] Updated exchange rates.')
-			currencyRates = rates
-		})
-	}
+	// function updateCurrencyRates() {
+	// 	getExchangeRates().then((rates) => {
+	// 		console.info("[INFO] Updated exchange rates.");
+	// 		currencyRates = rates;
+	// 	});
+	// }
 
-	onMount(() => {
-		const onMinute = 1 * 60 * 1000
-		updateCurrencyRates()
-		setInterval(updateCurrencyRates, onMinute)
-	})
+	// onMount(() => {
+	// 	const onMinute = 1 * 60 * 1000;
+	// 	updateCurrencyRates();
+	// 	setInterval(updateCurrencyRates, onMinute);
+	// });
 
 	afterUpdate(() => {
-		if (
-			   capacityFargate < 0
-			|| capacityFargateSpot < 0
-			|| timeValue < 0
-		) {
-			return
+		if (capacityFargate < 0 || capacityFargateSpot < 0 || timeValue < 0) {
+			return;
 		}
 
 		const getPrices = createPriceGetter({
 			region,
 			time: {
 				value: timeValue,
-				type: timeType
+				type: timeType,
 			},
-			exchangeRate: currencyRates[currency],
+			exchangeRate: 1,
 			cpu,
 			ram,
-		})
+		});
 
-		const fargatePrice = getPrices(capacityFargate, 'FARGATE')
-		const fargateSpotPrice = getPrices(capacityFargateSpot, 'FARGATE_SPOT')
+		const fargatePrice = getPrices(capacityFargate, "FARGATE");
+		const fargateSpotPrice = getPrices(capacityFargateSpot, "FARGATE_SPOT");
 
 		prices = {
 			total: fargatePrice.total + fargateSpotPrice.total,
 			fargatePrice,
 			fargateSpotPrice,
-		}
-	})
+		};
+	});
 </script>
 
-<style>
-	.main {
-		margin: 0 auto;
-	}
-	.main {
-		display: flex;
-        width: 900px;
-        margin: 0 auto;
-        padding: 10px 40px;
-        background-color: white;
-        box-shadow: 1px 1px 1px 1px #ccc;
-        border-radius: 2px;
-	}
-	.main > * {
-		flex: 1;
-        margin: 50px 20px;
-	}
-	.estimate {
-		text-align: center;
-	}
-	.estimate-price {
-		margin: 25px 0;
-		font-size: 3em;
-        font-family: 'Roboto Mono', monospace;
-	}
-</style>
-
 <div class="container">
-	<Header
-		bind:region={region}
-		bind:currency={currency}
-		currencyList={Object.keys(currencyRates)}
-	/>
+	<Header bind:region bind:currency currencyList={Object.keys(currencyRates)} />
 	<main class="main">
 		<form class="form">
 			<TimeInput bind:value={timeValue} bind:type={timeType} />
 			<CPUInput bind:value={cpu} />
-			<RAMInput bind:value={ram} cpu={cpu} />
+			<RAMInput bind:value={ram} {cpu} />
 			<CapacityInput bind:value={capacityFargate} name="FARGATE" />
 			<CapacityInput bind:value={capacityFargateSpot} name="FARGATE_SPOT" />
 		</form>
@@ -134,14 +99,14 @@
 			</h1>
 			<Table
 				name="FARGATE"
-				currency={currency}
+				{currency}
 				cpu={prices.fargatePrice.cpu}
 				ram={prices.fargatePrice.ram}
 				total={prices.total}
 			/>
 			<Table
 				name="FARGATE_SPOT"
-				currency={currency}
+				{currency}
 				cpu={prices.fargateSpotPrice.cpu}
 				ram={prices.fargateSpotPrice.ram}
 				total={prices.total}
@@ -149,3 +114,30 @@
 		</div>
 	</main>
 </div>
+
+<style>
+	.main {
+		margin: 0 auto;
+	}
+	.main {
+		display: flex;
+		width: 900px;
+		margin: 0 auto;
+		padding: 10px 40px;
+		background-color: white;
+		box-shadow: 1px 1px 1px 1px #ccc;
+		border-radius: 2px;
+	}
+	.main > * {
+		flex: 1;
+		margin: 50px 20px;
+	}
+	.estimate {
+		text-align: center;
+	}
+	.estimate-price {
+		margin: 25px 0;
+		font-size: 3em;
+		font-family: "Roboto Mono", monospace;
+	}
+</style>
